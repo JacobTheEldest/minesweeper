3796611083f958b58afa34c2b02147d24b5582c3
Jacob S. Steward <jacobtheeldest@gmail.com>
2023-04-20 22:50:20 -0400

    Prettier and eslint with gh actions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
-import Board from './components/Board';
 import { createContext, useEffect, useState } from 'react';
+import Board from './components/Board';
 
 export const BoardContext = createContext();
 
