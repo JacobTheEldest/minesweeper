4fbd1af60f36fb41114c6d744e7418f33bcc2daa
Jacob S. Steward <jacobtheeldest@gmail.com>
2023-05-17 16:54:23 -0400

    fix: add vite definition for APP_VERSION from package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
   server: {
     host: '0.0.0.0',
     port: 3000,
